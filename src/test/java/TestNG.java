f7ac7d6ecd2b0e803f0e0b1f334134297d722077
Agrawal, Abhishek (Agoda) <abhishek.agrawal@agoda.com>
2020-04-11 11:30:56 +0700

    change getData name

--- src/test/java/TestNG.java
+++ src/test/java/TestNG.java
@@ -3,9 +3,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import util.ReadData;
@@ -14,33 +17,54 @@ import java.nio.file.Paths;
 
 public class TestNG extends LoginPage {
 
+    //declare objects
     public WebDriver driver;
     ReadData dataList = new ReadData();
+    SoftAssert softassert = new SoftAssert();
+    WebDriverWait wait;
+
+    //ObjectRepositories
+    String loginTextFieldXpath = "//*[@id = 'email']";
+    String passwordTextFieldXpath = "//*[@id = 'pass']";
+    String profileTextXpath = "//*[@title='Profile']//*[text()='Kalam']";
+    
+    //Constants
+    String facebookEmailId = "kalamtechcorp@gmail.com";
+    String facebookPassword = "Vivek@Abhi2020";
+
 
 
     @Test
     public void facebookLogin() {
-        driver.get("https://www.facebook.com/");
-        WebElement loginTextField = driver.findElement(By.xpath("//*[@id = 'email']"));
-        loginTextField.sendKeys("kalamtechcorp@gmail.com");
+
+        //loginFLow
+        WebElement loginTextField = driver.findElement(By.xpath(loginTextFieldXpath));
+        loginTextField.sendKeys(facebookEmailId);
         WebElement passwordTextField = driver.findElement(By.xpath("//*[@id = 'pass']"));
-        passwordTextField.sendKeys("Vivek@Abhi2020");
+        passwordTextField.sendKeys(facebookPassword);
         passwordTextField.submit();
 
-        WebElement profileText = driver.findElement(By.xpath(""));
+        //ExpectHomePage
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(profileTextXpath)));
+        WebElement profileText = driver.findElement(By.xpath(profileTextXpath));
+        softassert.assertEquals (true, profileText.isDisplayed());
 
+        //FindRequiredPost
 
+        //Start Sharing
+        
     }
 
     @BeforeClass
     public void beforeClass() {
         System.setProperty("webdriver.gecko.driver", Paths.get("").toAbsolutePath().toString()+"//jars//geckodriver");
         driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterClass
     public void afterClass() {
         driver.quit();
     }
-
 }
