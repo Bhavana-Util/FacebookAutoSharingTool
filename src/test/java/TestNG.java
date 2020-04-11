3f381ddd24423f6cdd59cb3a33f5456aec2cfd8d
Agrawal, Abhishek (Agoda) <abhishek.agrawal@agoda.com>
2020-04-11 10:34:16 +0700

    Initial commit

---
+++ src/test/java/TestNG.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+
+import java.nio.file.Paths;
+
+public class TestNG extends LoginPage {
+
+    public WebDriver driver;
+
+    @Test
+    public void facebookLogin() {
+        driver.get("https://www.facebook.com/");
+        WebElement loginTextField = driver.findElement(By.xpath("//*[@id = 'email']"));
+        loginTextField.sendKeys("kalamtechcorp@gmail.com");
+        WebElement passwordTextField = driver.findElement(By.xpath("//*[@id = 'pass']"));
+        passwordTextField.sendKeys("Vivek@Abhi2020");
+        passwordTextField.submit();
+
+        WebElement profileText = driver.findElement(By.xpath(""));
+
+
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webdriver.gecko.driver", Paths.get("").toAbsolutePath().toString()+"//jars//geckodriver");
+        driver = new FirefoxDriver();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/util/ReadExcel.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.util.ArrayList;
+
+public class ReadExcel {
+
+
+    public ArrayList getData(){
+
+        //TODO read enum/excel & store all group name in ArrayList
+        ArrayList<String> facebookGroupList=new ArrayList<String>();
+
+
+        return facebookGroupList;
+    }
+
+
+}
